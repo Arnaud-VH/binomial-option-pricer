9ec05603da0c8433e96a0c08789838da1993e861
Arnaud-VH <arnaud.vanhees@icloud.com>
2025-12-04 21:14:22 +0000

    Adding visualizer, synching with Davide

---
+++ source/visualizer.py
@@ -0,0 +1,22 @@
+"""
+Module for the visualizing of binomial option pricing trees and other results. 
+"""
+
+class BinomialTreeVisualizer():
+   """
+   Class that creates visualizations of the binomial trees that are created in the binomial option pricing model. 
+   """
+
+   def __init__():
+      """
+      Constructor of BinomialTreeVisualizer class. The class does not have any state information. 
+      """
+      pass
+
+   def plot_small_tree(self, binomial_tree, max_steps = 4):
+      """
+      Visualization of small binomial tree. Default steps is 4. 
+      """
+
+
+   
